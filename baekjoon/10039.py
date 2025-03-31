904aff616363b8abb65d8e1594b1db572cc86d97
achv1223 <achv1223@gmail.com>
2025-03-31 20:00:37 +0900

    baekjoon 10039
    10039번 문제 제출입니다

---
+++ baekjoon/10039.py
@@ -0,0 +1,19 @@
+w=int(input())
+s=int(input())
+g=int(input())
+i=int(input())
+a=int(input())
+if w<=40:
+    w=40
+if s<=40:
+    s=40
+if g<=40:
+    g=40
+if i<=40:
+    i=40
+if a<=40:
+    a=40
+print((w+s+g+i+a)//5)
+    
+        
+    
