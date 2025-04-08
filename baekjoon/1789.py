c0f12b017c333183df36d169d496fa66c77ce2e1
achv1223 <achv1223@gmail.com>
2025-04-08 17:03:25 +0900

    baekjoon 1789
    1789번 문제 제출입니다

---
+++ baekjoon/1789.py
@@ -0,0 +1,13 @@
+#1789.소인수분해
+N=int(input())
+def prime_factorization(n):
+    fac=[]
+    for i in range(2,n+1):
+        while n%i==0:
+            fac.append(i)
+            n=n//i
+    return fac
+result=prime_factorization(N)
+for i in range(len(result)):
+    print(result[i], end=' ')
+    
